17e5f82bd04cae2558cac9773f610fe92907db99
didiermu <didiermu3@gmail.com>
2021-04-06 16:42:38 -0500

    dia del niño

---
+++ dist/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <link rel="preload" href="css/styles.min.css" as="style">
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <title>OXXO </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.min.css">
+    <script src="js/jquery-3.4.1.min.js"> </script>
+    <script src="js/bootstrap.min.js"> </script>
+    <script src="js/scripts.min.js"> </script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <body> 
+      <main class="homeNino">
+        <div id="particles-js" class="particulas"></div>
+        <div id="particles-js2" class="particulas"></div>
+        <div id="particles-js3" class="particulas"></div>
+        <div id="particles-js4" class="particulas"></div>
+        <section class="homeNino--hero">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-12">
+                <picture> 
+                  <source media="(min-width: 992px)" srcset="../img/STOREFRONT-1920x500.jpg"><img src="../img/STOREFRONT-544X440.jpg" alt="" class="img-fluid">
+                </picture>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="homeNino--content"> <img src="../img/HELADO-min.png" alt="" class="img-fluid helados"><img src="../img/HELADO-min.png" alt="" class="img-fluid helados">
+          <div class="container">
+            <div class="row"> 
+              <div class="col-12"> 
+                <h1> <b>Feliz Día del Niño a chicos y grande </b></h1>
+                <h2> <i>Festejemos con estos dulces y divertidos cupones </i></h2><img id="title" src="../img/title.png" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="homeNino--cupones">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-4 d-flex justify-content-center"><a href="#!" target="_blank" rel="noopener noreferrer" id="linkNaN"> <img src="../img/cupon.png" alt="" class="img-fluid"></a></div>
+              <div class="col-lg-4 d-flex justify-content-center"><a href="#!" target="_blank" rel="noopener noreferrer" id="linkNaN"> <img src="../img/cupon.png" alt="" class="img-fluid"></a></div>
+              <div class="col-lg-4 d-flex justify-content-center"><a href="#!" target="_blank" rel="noopener noreferrer" id="linkNaN"> <img src="../img/cupon.png" alt="" class="img-fluid"></a></div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </body>
+    <script src="js/particles.min.js"> </script>
+    <script src="js/app.js"> </script>
+  </head>
+</html>
